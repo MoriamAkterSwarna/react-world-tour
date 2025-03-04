5ec39d9c0e1367cee61d9e32a56fb3956d98817f
Moriam Akter Swarna <moriam15-12880@diu.edu.bd>
2025-03-04 11:44:40 +0600

    Code For Video 40-3 added

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -3,11 +3,15 @@ import "./Country.css";
 const Country = ({ country }) => {
   console.log(country);
 
-  const { name, flags } = country;
+  const { name, flags, population, area } = country;
   return (
     <div className="country">
       <h3>Name: {name?.common} </h3>
       <img src={flags.png} alt="" />
+
+      {/* For Video 40-3 */}
+      <p>Population: {population} </p>
+      <p>area: {area} </p>
     </div>
   );
 };
